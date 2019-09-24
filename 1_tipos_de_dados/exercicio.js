80fbe8f1ba8a7c38fb1f21014460adaadb8fb374
KateLucena <kate.lucena2011@gmail.com>
2019-09-23 22:26:08 -0300

    Semana 3

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,16 @@
-//resposta aqui
+let nome = "Kate Lucena";
+let idade = 30;
+let produto = ["Produto1", "Produto2", "Produto3", "Produto4"];
+let novoUser = true;
+
+
+let pessoa = {
+    nome: nome,
+    idade: idade,
+    novoUser: novoUser, 
+    produto: produto,
+}
+
+console.log(pessoa);
+
+
